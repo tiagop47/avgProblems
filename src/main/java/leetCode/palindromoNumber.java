c624eeb400b0210a65634051af078d3acd28b3dd
tiago peixoto <8240128@estg.ipp.pt>
2025-04-21 17:53:08 +0100

    dois

--- src/main/java/leetCode/palindromoNumber.java
+++ src/main/java/leetCode/palindromoNumber.java
@@ -1,5 +1,23 @@
 package leetCode;
 
 public class palindromoNumber {
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
 
+    public static boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int numeroReversed = 0;
+
+        while(x > numeroReversed) {
+            numeroReversed = numeroReversed * 10 + x % 10;
+            x /=  10;
+        }
+    return x == numeroReversed || x == numeroReversed / 10;
+    }
 }
+
