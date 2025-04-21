dd55802d8be4ac793e44432a6ecd353f75178f56
tiago peixoto <8240128@estg.ipp.pt>
2025-04-21 17:59:39 +0100

    resolvido

--- src/main/java/leetCode/palindromoNumber.java
+++ src/main/java/leetCode/palindromoNumber.java
@@ -2,11 +2,15 @@ package leetCode;
 
 public class palindromoNumber {
     public static void main(String[] args) {
-        int x = 121;
-        System.out.println(isPalindrome(x));
+
+        int x = 131;
+        palindromoNumber confirmar = new palindromoNumber();
+        boolean confirma = confirmar.isPalindrome(x);
+
+        System.out.println(confirma);
     }
 
-    public static boolean isPalindrome(int x) {
+    public  boolean isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
